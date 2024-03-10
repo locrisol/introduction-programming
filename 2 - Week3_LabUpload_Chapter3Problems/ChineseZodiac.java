d7fd0b4318d1524544e6ec437570eb170f6593f9
Leandro Crisol <leandroclpresti@gmail.com>
2024-03-10 13:18:16 +0000

    Update ChineseZodiac.java

--- 2 - Week3_LabUpload_Chapter3Problems/ChineseZodiac.java
+++ 2 - Week3_LabUpload_Chapter3Problems/ChineseZodiac.java
@@ -4,9 +4,11 @@
 * This program prints the corresponding animal from the Chinese
 *
 *
-* author  Leandro Crisol Lo Presti
-* version 1.0
-* since   15/02/2024
+* author            Leandro Crisol Lo Presti
+* version           1.0
+* since             15/02/2024
+* @module           Introduction to Programming (BSHCE1)
+* @student_number   23156503
 */
 
 import java.util.Scanner;
